e12194bbd49e9c49af33296047f6424a4d3ce638
Randalthor95 <Randalthor95@gmail.com>
2021-11-16 21:38:16 -0500

    CNN right shapes, not training

--- Final_Project/finalprojectneuralnetworks.py
+++ Final_Project/finalprojectneuralnetworks.py
@@ -90,7 +90,7 @@ class NeuralNetwork(nn.Module):
 
 class CNN(nn.Module):
     def __init__(self, num_channels, num_inputs, num_hiddens_per_conv_layer, num_hiddens_per_fc_layer, num_outputs,
-                 kernel_size_per_conv_layer, stride_per_conv_layer):
+                 kernel_size_per_conv_layer, stride_per_conv_layer, activation_function="tanh"):
         super(CNN, self).__init__()
         n_conv_layers = len(num_hiddens_per_conv_layer)
         self.num_inputs = num_inputs
@@ -103,104 +103,142 @@ class CNN(nn.Module):
                 "be equal. "
             )
 
-        # self.flatten = nn.Flatten()
-        self.odc = OrderedDict([])
-        self.fcod = OrderedDict([])
-        if num_hiddens_per_conv_layer:
-            for i in range(len(num_hiddens_per_conv_layer)):
-                print("i: ", i)
-                if i == 0:
-                    self.odc['Conv1d0'] = nn.Sequential(
-                        nn.Conv1d(num_channels, num_hiddens_per_conv_layer[0],
-                                  kernel_size=kernel_size_per_conv_layer[0],
-                                  stride=stride_per_conv_layer[0]),
-                        nn.ReLU())
-                    liner_input = ((num_inputs - (kernel_size_per_conv_layer[len(kernel_size_per_conv_layer) - 1] - 1)
-                                    * len(num_hiddens_per_conv_layer)) // (stride_per_conv_layer[i])) * \
-                                  num_hiddens_per_conv_layer[
-                                      len(num_hiddens_per_conv_layer) - 1]
-                    sum = 0
-                    for i in range(len(kernel_size_per_conv_layer) - 1):
-                        print("i: ", i)
-                        print("stride_per_conv_layer:", stride_per_conv_layer[i])
-                        print(((kernel_size_per_conv_layer[i] - kernel_size_per_conv_layer[
-                            len(kernel_size_per_conv_layer) - 1]) // (stride_per_conv_layer[i])) \
-                              * num_hiddens_per_conv_layer[len(num_hiddens_per_conv_layer) - 1])
-                        sum += ((kernel_size_per_conv_layer[i] - kernel_size_per_conv_layer[
-                            len(kernel_size_per_conv_layer) - 1]) // (stride_per_conv_layer[i])) \
-                               * num_hiddens_per_conv_layer[len(num_hiddens_per_conv_layer) - 1]
-                    liner_input -= sum
-                    print("num_inputs", num_inputs)
-                    print(kernel_size_per_conv_layer[len(kernel_size_per_conv_layer) - 1] - 1)
-                    print(len(num_hiddens_per_conv_layer))
-                    print(num_hiddens_per_conv_layer[
-                              len(num_hiddens_per_conv_layer) - 1])
-                    print(sum)
-                    print("liner_input: ", liner_input)
-                    self.fcod['Linear0'] = nn.Sequential(
-                        nn.Linear(liner_input, num_hiddens_per_fc_layer[0]),
-                        nn.ReLU())
-                    # self.odc['ReLU0'] = nn.ReLU()
-                else:
-                    self.odc['Conv1d' + str(i)] = nn.Sequential(
-                        nn.Conv1d(num_hiddens_per_conv_layer[i - 1],
-                                  num_hiddens_per_conv_layer[i],
-                                  kernel_size=kernel_size_per_conv_layer[i],
-                                  stride=stride_per_conv_layer[i]),
-                        nn.ReLU())
-                    self.fcod['Linear' + str(i)] = nn.Sequential(
-                        nn.Linear(num_hiddens_per_fc_layer[i - 1],
-                                  num_hiddens_per_fc_layer[i]),
-                        nn.ReLU())
-            self.last_layer = nn.Linear(num_hiddens_per_fc_layer[len(num_hiddens_per_fc_layer) - 1],
-                                        num_outputs)
-            # self.odc['ReLU' + str(i)] = nn.ReLU()
+        if activation_function == "tanh":
+            self.activation_function = torch.nn.Tanh
+        elif activation_function == "softplus":
+            self.activation_function = torch.nn.Softplus
+        else:
+            self.activation_function = torch.nn.ReLU
 
-        # self.od = OrderedDict([])
-        # if num_hiddens_per_fc_layer:
-        #     for i in range(len(num_hiddens_per_fc_layer)):
+        # Create all convolutional layers
+        # First argument to first Conv2d is number of channels for each pixel.
+        # Just 1 for our grayscale images.
+        n_in = 1
+        input_w = num_inputs  # original input image height (=width because image assumed square)
+        self.conv_layers = torch.nn.ModuleList()
+
+        for nh, patch_size, stride in zip(
+                num_hiddens_per_conv_layer, kernel_size_per_conv_layer, stride_per_conv_layer
+        ):
+            self.conv_layers.append(
+                torch.nn.Conv1d(n_in, nh, kernel_size=patch_size, stride=stride)
+            )
+            conv_layer_output_hw = (input_w - patch_size) // stride + 1
+            input_w = conv_layer_output_hw  # for next trip through this loop
+            n_in = nh
+
+        # Create all fully connected layers.  First must determine number of inputs to first
+        # fully-connected layer that results from flattening the images coming out of the last
+        # convolutional layer.
+        n_in = input_w * n_in  # n_hiddens_per_fc_layer[0]
+        self.fc_layers = torch.nn.ModuleList()
+        for nh in num_hiddens_per_fc_layer:
+            self.fc_layers.append(torch.nn.Linear(n_in, nh))
+            n_in = nh
+        self.fc_layers.append(torch.nn.Linear(n_in, num_outputs))
+
+        # self.conv_layers = torch.nn.ModuleList()
+        # self.fc_layers = torch.nn.ModuleList()
+        # self.cod = OrderedDict([])
+        # self.fcod = OrderedDict([])
+        # if num_hiddens_per_conv_layer:
+        #     for i in range(len(num_hiddens_per_conv_layer)):
+        #         print("i: ", i)
         #         if i == 0:
-        #             print('i == 0')
-        #             self.od['Linear0'] = nn.Linear(num_hiddens_per_conv_layer[len(num_hiddens_per_conv_layer)-1], num_hiddens_per_fc_layer[0])
-        #             # self.od['ReLU0'] = nn.ReLU()
+        #             self.conv_layers.append(
+        #                 nn.Conv1d(num_channels, num_hiddens_per_conv_layer[0],
+        #                           kernel_size=kernel_size_per_conv_layer[0],
+        #                           stride=stride_per_conv_layer[0])
+        #             )
+        #             # self.cod['Conv1d0'] = nn.Sequential(
+        #             #     nn.Conv1d(num_channels, num_hiddens_per_conv_layer[0],
+        #             #               kernel_size=kernel_size_per_conv_layer[0],
+        #             #               stride=stride_per_conv_layer[0]),
+        #             #     nn.ReLU())
+        #             liner_input = ((num_inputs - (kernel_size_per_conv_layer[len(kernel_size_per_conv_layer) - 1] - 1)
+        #                             * len(num_hiddens_per_conv_layer)) // (stride_per_conv_layer[i])) * \
+        #                           num_hiddens_per_conv_layer[
+        #                               len(num_hiddens_per_conv_layer) - 1]
+        #             sum = 0
+        #             for i in range(len(kernel_size_per_conv_layer) - 1):
+        #                 print("i: ", i)
+        #                 print("stride_per_conv_layer:", stride_per_conv_layer[i])
+        #                 print(((kernel_size_per_conv_layer[i] - kernel_size_per_conv_layer[
+        #                     len(kernel_size_per_conv_layer) - 1]) // (stride_per_conv_layer[i])) \
+        #                       * num_hiddens_per_conv_layer[len(num_hiddens_per_conv_layer) - 1])
+        #                 sum += ((kernel_size_per_conv_layer[i] - kernel_size_per_conv_layer[
+        #                     len(kernel_size_per_conv_layer) - 1]) // (stride_per_conv_layer[i])) \
+        #                        * num_hiddens_per_conv_layer[len(num_hiddens_per_conv_layer) - 1]
+        #             liner_input -= sum
+        #             print("num_inputs", num_inputs)
+        #             print(kernel_size_per_conv_layer[len(kernel_size_per_conv_layer) - 1] - 1)
+        #             print(len(num_hiddens_per_conv_layer))
+        #             print(num_hiddens_per_conv_layer[
+        #                       len(num_hiddens_per_conv_layer) - 1])
+        #             print(sum)
+        #             print("liner_input: ", liner_input)
+        #
+        #             self.fc_layers.append(nn.Linear(liner_input, num_hiddens_per_fc_layer[0]))
+        #
+        #             # self.fcod['Linear0'] = nn.Sequential(
+        #             #     nn.Linear(liner_input, num_hiddens_per_fc_layer[0]),
+        #             #     nn.ReLU())
+        #             # self.cod['ReLU0'] = nn.ReLU()
         #         else:
-        #             self.od['Linear' + str(i)] = nn.Linear(num_hiddens_per_fc_layer[i - 1], num_hiddens_per_fc_layer[i])
-        #             # self.od['ReLU' + str(i)] = nn.ReLU()
-        #     self.od['Linear' + str(len(num_hiddens_per_fc_layer))] = \
-        #         nn.Linear(num_hiddens_per_fc_layer[len(num_hiddens_per_fc_layer) - 1], num_outputs)
-        # else:
-        #     self.od['Linear0'] = nn.Linear(num_inputs, num_outputs)
-
-        # self.conv1d_linear_stack = nn.Sequential(od)
+        #             self.conv_layers.append(nn.Conv1d(num_hiddens_per_conv_layer[i - 1],
+        #                           num_hiddens_per_conv_layer[i],
+        #                           kernel_size=kernel_size_per_conv_layer[i],
+        #                           stride=stride_per_conv_layer[i]))
+        #             self.fc_layers.append(nn.Linear(num_hiddens_per_fc_layer[i - 1],
+        #                                             num_hiddens_per_fc_layer[i]))
+        #
+        #             # self.conv_layers.append(nn.Conv1d(num_hiddens_per_conv_layer[i - 1],
+        #             #               num_hiddens_per_conv_layer[i],
+        #             #               kernel_size=kernel_size_per_conv_layer[i],
+        #             #               stride=stride_per_conv_layer[i]))
+        #             # self.cod['Conv1d' + str(i)] = nn.Sequential(
+        #             #     nn.Conv1d(num_hiddens_per_conv_layer[i - 1],
+        #             #               num_hiddens_per_conv_layer[i],
+        #             #               kernel_size=kernel_size_per_conv_layer[i],
+        #             #               stride=stride_per_conv_layer[i]),
+        #             #     nn.ReLU())
+        #
+        #
+        #             # self.fcod['Linear' + str(i)] = nn.Sequential(
+        #             #     nn.Linear(num_hiddens_per_fc_layer[i - 1],
+        #             #               num_hiddens_per_fc_layer[i]),
+        #             #     nn.ReLU())
+        #
+        #     self.last_layer = nn.Linear(num_hiddens_per_fc_layer[len(num_hiddens_per_fc_layer) - 1],
+        #                                 num_outputs)
+        #     # self.cod['ReLU' + str(i)] = nn.ReLU()
+        #
+        # # self.od = OrderedDict([])
+        # # if num_hiddens_per_fc_layer:
+        # #     for i in range(len(num_hiddens_per_fc_layer)):
+        # #         if i == 0:
+        # #             print('i == 0')
+        # #             self.od['Linear0'] = nn.Linear(num_hiddens_per_conv_layer[len(num_hiddens_per_conv_layer)-1], num_hiddens_per_fc_layer[0])
+        # #             # self.od['ReLU0'] = nn.ReLU()
+        # #         else:
+        # #             self.od['Linear' + str(i)] = nn.Linear(num_hiddens_per_fc_layer[i - 1], num_hiddens_per_fc_layer[i])
+        # #             # self.od['ReLU' + str(i)] = nn.ReLU()
+        # #     self.od['Linear' + str(len(num_hiddens_per_fc_layer))] = \
+        # #         nn.Linear(num_hiddens_per_fc_layer[len(num_hiddens_per_fc_layer) - 1], num_outputs)
+        # # else:
+        # #     self.od['Linear0'] = nn.Linear(num_inputs, num_outputs)
+        #
+        # # self.conv1d_linear_stack = nn.Sequential(od)
 
     def forward(self, x):
+        for conv_layer in self.conv_layers:
+            x = conv_layer(x)
 
-        for conv_layer in self.odc:
-            print("x.shape1: ", x.shape)
-            print("self.odc[conv_layer]: ", self.odc[conv_layer])
-            x = self.odc[conv_layer](x)
-        print("x.shape2: ", x.shape)
-        # out = x.view(x_shape_zero, x.size(1))
         x = torch.flatten(x, 1)  # flatten all dimensions except batch
-        for linear_layer in self.fcod:
-            print("x.shape1: ", x.shape)
-            print("self.fcod[conv_layer]: ", self.fcod[linear_layer])
-            x = self.fcod[linear_layer](x)
-        logit = self.last_layer(x)
-        return logit
-        # x = x.unsqueeze(dim=0)
-        # for conv_layer in self.odc:
-        #     print(self.odc[conv_layer])
-        #     x = F.relu(self.odc[conv_layer](x))
-        #     print(x.size())
-        #
-        # # x = torch.flatten(x, 0)  # flatten all dimensions except batch
-        # x = x.squeeze()
-        # for i, layer in enumerate(self.od):
-        #     print(self.od[layer])
-        #     if i == len(self.od) - 1:
-        #         x = self.od[layer](x)
-        #     else:
-        #         x = F.relu(self.od[layer](x))
-        #     print(print(x.size()))
-        # return x
+        for i, fc_layer in enumerate(self.fc_layers):
+            if i == len(self.fc_layers) - 1:
+                break
+            x = fc_layer(x)
+
+        x = self.fc_layers[-1](x)
+        return x
